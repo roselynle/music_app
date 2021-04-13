d884de2256da25a2eb232d074e59b8a2c152a11c
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-13 11:48:29 +0100

    adding LikeButton rendering text test

--- src/components/LikeButton/test.js
+++ src/components/LikeButton/test.js
@@ -1,3 +1,13 @@
 import { screen } from '@testing-library/react';
-
 import LikeButton from '.';
+
+describe('LikeButton', () => {
+    beforeEach(() => { 
+        render(<LikeButton />)
+    })
+
+    test('renders a button with a Like in it', () => {
+        let likeButton = screen.getByRole("switch")
+        expect(likeButton.textContent).toBe('Like')
+    })
+})
