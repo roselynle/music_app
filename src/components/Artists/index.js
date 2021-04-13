d6e8bbf54c8678d82af836e086103d4403f94b17
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-13 12:08:57 +0100

    adding some more artists

--- src/components/Artists/index.js
+++ src/components/Artists/index.js
@@ -3,11 +3,11 @@
 //intro p
 import React, { useState } from 'react';
 
-const ArtistCard = () => {
-    const [ artistName, setArtistName ] = useState('Drake');
-    const [ artistGenre, setArtistGenre ] = useState('Hip Hop')
-    const [ artistIntro, setArtistIntro ] = useState('Aubrey Drake Graham is a Canadian rapper, singer, songwriter, actor, and entrepreneur. A prominent figure in popular music, Drake is credited for popularizing the Toronto sound.')
-
+const [ artists, setArtists ] = useState([
+    { id: 1, artistName: 'Drake', artistGenre: 'Hip Hop', artistIntro: 'Aubrey Drake Graham is a Canadian rapper, singer, songwriter, actor, and entrepreneur. A prominent figure in popular music, Drake is credited for popularizing the Toronto sound.'},
+    { id: 2, artistName: 'Michael Jackson', artistGenre: 'Pop, Hip Hop, R&B, Soul', artistIntro: 'Michael Joseph Jackson was an American singer, songwriter, and dancer. Dubbed the "King of Pop", he is regarded as one of the most significant cultural figures of the 20th century.'},
+    { id: 3, artistName: 'Eric Clapton', artistGenre: 'Blues, Rock', artistIntro: 'Eric Patrick Clapton is an English rock and blues guitarist, singer, and songwriter. Clapton has been referred to as one of the most important and influential guitarists of all time'}
+])
     return (
         <div>
             <h2>{artistName}</h2>
