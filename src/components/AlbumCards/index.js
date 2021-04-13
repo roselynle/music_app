7e36d49284f8e1cc54d660cddc866346e62c5e31
Roselyn Le <roselynle95@gmail.com>
2021-04-13 16:01:45 +0100

    refactor to use props

--- src/components/AlbumCards/index.js
+++ src/components/AlbumCards/index.js
@@ -1,31 +1,21 @@
-//songs
-//albums
-//name, release date, cover art
+import React, { useState } from "react";
+import { LikeButton } from "../";
 
-import React, { useState } from 'react';
+const AlbumCards = ({ album }) => {
+    const renderAlbumCards = () => {
+        return album.map((a) => (
+            <div key={a.id} className="card-id">
+                <div className="card">
+                    <img src={a.cover} className="albumImg" alt="Album Cover" />
+                    <h5 className="card-title">Name: {a.name}</h5>
+                    <p className="card-text">Release date: {a.release_date}</p>
+                    <LikeButton/>
+                </div>
+            </div>
+        ));
+    };
 
-const AlbumCards = () => {
-    const [album, setAlbum] = useState([
-        { name: 'Scorpion', release_date: '2018', cover: "https://upload.wikimedia.org/wikipedia/en/9/90/Scorpion_by_Drake.jpg"}
-    ])
-
-    const renderRows = () => {
-        return album.map(a => <tr><td>{a.name}</td><td>{a.release_date}</td><td><img src={a.cover} /></td></tr>)
-    }
-    return (
-        <table style={{ border: "1px solid black", width: "100vw", textAlign: "center" }}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Release Date</th>
-            <th>Cover</th>
-          </tr>
-        </thead>
-        <tbody>
-          { renderRows() }
-        </tbody>
-      </table> 
-    );
+    return <div>{renderAlbumCards()}</div>;
 };
 
-export default AlbumCards
+export default AlbumCards;
