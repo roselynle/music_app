20a2cd5a823f07b7dea7679189c2a607d8a19b58
Roselyn Le <roselynle95@gmail.com>
2021-04-13 16:00:31 +0100

    change artist component to artist bio

---
+++ src/components/ArtistBio/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ArtistBio() {
+    return (
+        <>
+            <div className="bio">
+                <h3>Hip Hop Artist</h3>
+                <p>
+                    Aubrey Drake Graham is a Canadian rapper, singer,
+                    songwriter, actor, and entrepreneur. A prominent figure in
+                    popular music, Drake is credited for popularizing the
+                    Toronto sound.
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default ArtistBio;
