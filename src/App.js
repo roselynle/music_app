05ef2096971105e33999808c09c289808c569178
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-14 12:09:50 +0100

    adding simple nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import * as Pages from './pages';
-import { Switch, Route } from 'react-router-dom';
+import { Header, Footer } from './layout';
+
 import './App.css';
 
 function App() {
     return (
      <>
+        <Header />
         <Switch>
            <Route exact path="/" component={Pages.Homepage} />
            <Route path="/about" component={Pages.About} />
            <Route path="/lyrics" component={Pages.Lyrics} />
            <Route component={Pages.NotFound} />
         </Switch>
+        <Footer />
      </>
     );
 }
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './style.css';
 
-const Header = () => <nav>Drake Appreciation Page</nav>;
-
+const Header = () => {
+     return (
+         <nav>
+            <NavLink exact to="/" activeClassName="current">Homepage</NavLink>
+            <NavLink exact to="/about" activeClassName="current">About</NavLink>
+            <NavLink exact to="/lyrics" activeClassName="current">Lyrics</NavLink>
+         </nav>
+          
+    );
+}
 export default Header;
