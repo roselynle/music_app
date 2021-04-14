83ac4bcb2709ca9b5c6317aee9f12e5bf841f24a
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-14 12:58:24 +0100

    adding Homepage tests

--- src/components/ArtistBio/index.js
+++ src/components/ArtistBio/index.js
@@ -6,7 +6,7 @@ function ArtistBio() {
             <div className="bio">
             <h1 id="heading">Drake Appreciation Club</h1>
                 <h2 id="genre">Hip Hop Artist</h2>
-                <p>
+                <p role="bio">
                     Aubrey Drake Graham is a Canadian rapper, singer,
                     songwriter, actor, and entrepreneur. A prominent figure in
                     popular music, Drake is credited for popularizing the
--- src/pages/Homepage/test.js
+++ src/pages/Homepage/test.js
@@ -1,13 +1,18 @@
 import { screen } from '@testing-library/react';
-
+import {MemoryRouter} from 'react-router-dom';
 import Homepage from '../Homepage';
 
-// describe('Homepage', () => {
-//     beforeEach(() => render(<Homepage />, { wrapper: MemoryRouter }));
+describe('Homepage', () => {
+    beforeEach(() => render(<Homepage />, { wrapper: MemoryRouter }));
 
-//     test("it displays greeting", () => {
-//         const greeting = screen.queryByRole('heading', {name: 'greeting'})
-//         expect(greeting).toBeInTheDocument();
-//     });
+    test("it displays greeting", () => {
+        const greeting = screen.queryByRole('heading', {name: 'greeting'})
+        expect(greeting).toBeInTheDocument();
+    });
 
-// })
+    test("page contains a bio", () => {
+        const bio = screen.queryByRole('bio')
+        expect(bio).toBeInTheDocument();
+    })
+
+})
