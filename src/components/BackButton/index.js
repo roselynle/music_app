43f3486c3067ff5f1ae45dba56e5c83092e8cfa1
Roselyn Le <roselynle95@gmail.com>
2021-04-15 01:13:36 +0100

    styling changes

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -4,7 +4,7 @@ import { useHistory } from 'react-router-dom';
 const BackButton = () => {
      const history = useHistory();
 
-     return <button id="back-button" onClick={history.goBack}>Back</button>
+     return <span id="back-button" onClick={history.goBack}><i className="fas fa-arrow-circle-left"></i></span>
 }
 
 export default BackButton
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import { BackButton } from '../../components';
-import './style.css';
+import { BackButton } from "../../components";
+import "./style.css";
 
 const Header = () => {
-     return (
-         <nav role='navigation'>
-            <NavLink exact to="/" activeClassName="current">Homepage</NavLink>
-            <NavLink exact to="/about" activeClassName="current">About</NavLink>
-            <NavLink exact to="/lyrics" activeClassName="current">Lyrics</NavLink>
-            <NavLink exact to="/albums" activeClassName="current">Albums</NavLink>
+    return (
+        <nav role="navigation">
             <BackButton />
-         </nav>
-          
+            <NavLink exact to="/" activeClassName="current">
+                Homepage
+            </NavLink>
+            <NavLink exact to="/about" activeClassName="current">
+                About
+            </NavLink>
+            <NavLink exact to="/lyrics" activeClassName="current">
+                Lyrics
+            </NavLink>
+            <NavLink exact to="/albums" activeClassName="current">
+                Albums
+            </NavLink>
+        </nav>
     );
-}
+};
 export default Header;
